45f281de7a9798a2596beb16a04f3689a4c566d1
Ignacio <ignavaldi@gmail.com>
2013-07-11 22:39:02 -0300

    modified grader to check urls

--- grader.js
+++ grader.js
@@ -22,10 +22,13 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('util');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://ancient-mountain-3385.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +64,28 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkFile = function(file) {
+    var checkJson = checkHtmlFile(file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_string>', 'Url to app', null, URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url) {
+      rest.get(program.url).on('complete', function(data) {
+        fs.writeFileSync('savedPage.html',data);
+        checkFile('savedPage.html'); 
+      });
+    }else{
+      checkFile(program.file);
+    }
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
